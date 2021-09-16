2e0e661d3d210c01a39a41a29b1d126a4db0b31b
Toby Higbie <higbie@history.ucla.edu>
2021-09-15 20:22:07 -0700

    first commit

---
+++ js/map.js
@@ -0,0 +1,65 @@
+// Global variables
+let map;
+// path to csv data
+let path = "data/la-sites-test.csv";
+let markers = L.featureGroup();
+
+// initialize
+$( document ).ready(function() {
+    createMap();
+	readCSV(path);
+});
+
+// create the map
+function createMap(){
+	map = L.map('map').setView([0,0],3);
+
+	L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+		attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+	}).addTo(map);
+}
+
+// function to read csv data
+function readCSV(){
+	Papa.parse(path, {
+		header: true,
+		download: true,
+		complete: function(data) {
+			console.log(data);
+			
+			// map the data
+			mapCSV(data);
+
+		}
+	});
+}
+
+function mapCSV(data){
+
+	// circle options
+	let circleOptions = {
+		radius: 7,
+		weight: 1,
+		color: 'white',
+		fillColor: 'dodgerblue',
+		fillOpacity: 1
+	}
+
+	// loop through each entry
+	data.data.forEach(function(item,index){
+		// create a marker
+		let marker = L.circleMarker([item.latitude,item.longitude],circleOptions)
+		.on('click',function(){
+			this.bindPopup(`<b>${item.title}</b><br>${item.description}<br>${item.address}<br><em>${item.theme}</em>`).openPopup()
+		})
+
+		// add marker to featuregroup
+		markers.addLayer(marker)
+	})
+
+	// add featuregroup to map
+	markers.addTo(map)
+
+	// fit map to markers
+	map.fitBounds(markers.getBounds())
+}
